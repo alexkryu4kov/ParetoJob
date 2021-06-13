fea02d1913216805b96c483a8a8b3b74627ed8bb
akrjuchkov <al.krjuchkov@cft.ru>
2021-06-13 14:59:14 +0700

    Улучшил сценарии

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 from telegram.ext import Filters
 from telegram.ext.callbackcontext import CallbackContext
@@ -17,6 +18,10 @@ from perfect_vacancy import (
 )
 
 
+def random_greeting():
+    return random.choice(['Отличный выбор.', 'Спасибо что поделился!', 'Записал в свой блокнот.'])
+
+
 updater = Updater(os.environ['TOKEN'], use_context=True)
 dispatcher = updater.dispatcher
 
@@ -26,12 +31,24 @@ professions = []
 def start(update: Update, context: CallbackContext):
     kbd_layout = [['Python разработчик'], ['Таргетолог'], ['UI/UX designer']]
     kbd = ReplyKeyboardMarkup(kbd_layout)
-    update.message.reply_text(text="Выбери профессию, в которой ты хочешь развиваться", reply_markup=kbd)
+    update.message.reply_text(text='Выбери профессию, в которой ты хочешь развиваться', reply_markup=kbd)
 
 
 def rules(update: Update, context: CallbackContext):
-    answer = f'Привет, я бот'
+    answer = f'Привет, меня зовут Парето! Я помогу тебе получить работу твоей мечты, приложив минимум усилий!\n' \
+             f'Для начала тебе надо будет немного рассказать о своих навыках\n' \
+             f'Потом искусственный интеллект подберет тебе идеальную вакансию, и расскажет какие курсы надо пройти для ее получения'
     update.message.reply_text(answer)
+
+
+def ask_salary(update: Update, context: CallbackContext):
+    kbd_layout = [['0-100'], ['100-150'], ['150+']]
+    kbd = ReplyKeyboardMarkup(kbd_layout)
+    update.message.reply_text(
+        text='Какая интересная профессия!\n'
+             'Укажи диапазон в котором находится твоя зарплата (об этом конечно же никто не узнает)',
+        reply_markup=kbd,
+    )
 
 
 def choose_perfect_skills(update: Update, context: CallbackContext):
@@ -40,11 +57,16 @@ def choose_perfect_skills(update: Update, context: CallbackContext):
     if update.message.text in ('Python', 'SQL', 'Web'):
         context.bot.sendMessage(
             chat_id=update.message.chat_id,
-            text=f'Отличный выбор! Есть ли еще навыки, в которых ты уверен?',
+            text=f'{random_greeting()} Есть ли еще навыки, в которых ты уверен?',
             reply_markup=kbd,
         )
     else:
-        update.message.reply_text(text="Выбери навыки, в которых ты уверен", reply_markup=kbd)
+        update.message.reply_text(
+            text='Благодарю за честность!\n'
+                 'А теперь давай познакомимся поближе:)\n'
+                 'Выбери навыки из списка ниже, в которых ты уверен',
+            reply_markup=kbd,
+        )
 
 
 def choose_middle_skills(update: Update, context: CallbackContext):
@@ -53,11 +75,15 @@ def choose_middle_skills(update: Update, context: CallbackContext):
     if update.message.text in ('CI/CD', 'Golang', 'Tests'):
         context.bot.sendMessage(
             chat_id=update.message.chat_id,
-            text=f'Отличный выбор! Есть ли еще навыки, которые хочется подтянуть?',
+            text=f'{random_greeting()} Есть ли еще навыки, которые хочется подтянуть?',
             reply_markup=kbd,
         )
     else:
-        update.message.reply_text(text="Выбери навыки, которые хочется подтянуть", reply_markup=kbd)
+        update.message.reply_text(
+            text='Отлично, переходим к следующему этапу!\n'
+                 'Выбери навыки из спика ниже, которые хочется подтянуть',
+            reply_markup=kbd,
+        )
 
 
 def choose_weak_skills(update: Update, context: CallbackContext):
@@ -66,27 +92,46 @@ def choose_weak_skills(update: Update, context: CallbackContext):
     if update.message.text in ('Teamwork', 'Docker', 'Asyncio'):
         context.bot.sendMessage(
             chat_id=update.message.chat_id,
-            text='Отличный выбор! Есть ли еще навыки, которые хочется изучить?',
+            text=f'{random_greeting()} Есть ли еще навыки, которые хочется изучить?',
             reply_markup=kbd,
         )
     else:
-        update.message.reply_text(text="Выбери навыки, которые хочется изучить", reply_markup=kbd)
+        update.message.reply_text(
+            text='Осталось совсем чуть-чуть!\n'
+                 'Выбери навыки из спика ниже, которые хочется изучить',
+            reply_markup=kbd,
+        )
 
 
 def make_recommendation(update: Update, context: CallbackContext):
-    reply_markup = ReplyKeyboardRemove()
+    kbd_layout = [['А как мне его подтянуть']]
+    kbd = ReplyKeyboardMarkup(kbd_layout, resize_keyboard=True)
     perfect_vacancy = get_perfect_vacancy(person_description, all_vacancies_descriptions)
     worst_skill, skill_difference = get_skill_difference(person_description, perfect_vacancy)
-    answer = f'Вакансия для тебя: {perfect_vacancy.name} {perfect_vacancy.link}\n' \
-             f'Прирост в зарплате составит: {perfect_vacancy.salary - person_description.salary}\n' \
+    answer = f'Моя рекомендация готова!\n\n' \
+             f'Вакансия для тебя: {perfect_vacancy.name} {perfect_vacancy.link}\n' \
+             f'Прирост в зарплате составит примерно {round((perfect_vacancy.salary - person_description.salary)/person_description.salary, 2) * 100}% от текущей зарплаты\n' \
              f'Скилл, который нужно подтянуть в первую очередь: {worst_skill}. ' \
-             f'Разница с идеалом составляет {skill_difference}'
+             f'Разница с идеалом составляет {skill_difference} условных пункта'
+    update.message.reply_text(text=answer, reply_markup=kbd)
+
+
+def courses(update: Update, context: CallbackContext):
+    reply_markup = ReplyKeyboardRemove()
+    answer = f'Обрати внимание на эти ресурсы:\n' \
+             f'Курс: https://netology.ru/programs/sql-lessons\n' \
+             f'Книга: https://www.oreilly.com/library/view/head-first-sql/9780596526849\n'
     update.message.reply_text(text=answer, reply_markup=reply_markup)
 
 
 dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('help', rules))
 dispatcher.add_handler(MessageHandler(
-    Filters.regex('^(Python разработчик|Таргетолог|UI/UX designer|Python|SQL|Web)$'),
+    Filters.regex('^(Python разработчик|Таргетолог|UI/UX designer)$'),
+    ask_salary
+))
+dispatcher.add_handler(MessageHandler(
+    Filters.regex('^(0-100|100-150|150+|Python|SQL|Web)$'),
     choose_perfect_skills,
 ))
 dispatcher.add_handler(MessageHandler(
@@ -100,6 +145,10 @@ dispatcher.add_handler(MessageHandler(
 dispatcher.add_handler(MessageHandler(
     Filters.regex('^Перейти к рекомендациям$'),
     make_recommendation,
+))
+dispatcher.add_handler(MessageHandler(
+    Filters.regex('^А как мне его подтянуть$'),
+    courses,
 ))
 dispatcher.add_handler(MessageHandler(None, start))
 updater.start_polling()
